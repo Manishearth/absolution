e2753610a4ded990bfb6145bce1e59fb9253fa6d
Manish Goregaokar <manishsmail@gmail.com>
2020-03-20 10:59:01 -0700

    Add roundtrip tests

---
+++ tests/roundtrip.rs
@@ -0,0 +1,22 @@
+use proc_macro2;
+use absolution::TokenStream;
+use quote::{quote, ToTokens};
+
+fn test_roundtrip(pm: proc_macro2::TokenStream) {
+    let ab: TokenStream = pm.clone().into();
+    assert_eq!(pm.to_string(), ab.to_token_stream().to_string());
+    assert_eq!(pm.to_string(), quote!(#ab).to_string());
+}
+
+#[test]
+fn roundtrip_basic() {
+    test_roundtrip(quote!(1));
+    test_roundtrip(quote!("foo"));
+    test_roundtrip(quote!("foo" + 5));
+    test_roundtrip(quote!(5u8));
+    test_roundtrip(quote!(5f32));
+    test_roundtrip(quote!("hello"));
+    test_roundtrip(quote!(1 2 3 -1));
+    test_roundtrip(quote!(1 + (1 - five)));
+    test_roundtrip(quote!(1 += 3 (foo("hi"))));
+}
